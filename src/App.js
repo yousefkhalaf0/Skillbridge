c7d4c6adb107683794481bbe2c7281a147c0e151
yousefkhalaf0 <yousefkhalaf667@gmail.com>
2025-01-31 14:48:25 +0200

    add routing with BrowserRouter and initial Home route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <main>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
